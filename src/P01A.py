b123a3de9b7bc5e676d7439f635de212a395f845
CharlieC316 <chcarnahan@gmail.com>
2025-10-22 16:57:10 -0500

    added everything that will build the sword

--- src/P01A.py
+++ src/P01A.py
@@ -4,8 +4,21 @@ class Sword:
     
     def __init__(self):
         #parameters you can customize
-        # TODO
-        # Blade Length 
-        # Blade Width
-        # Blade Thinkness 
-        # Gaurd Height and weight 
+        # Blade
+       self.blade_length = 5.0
+       self.blade_width = 0.2
+       self.blade_thickness = 0.1
+
+       # Gaurd
+       self.gaurd_width = 1.2
+       self.gaurd_thickness = 0.1
+       self.gaurd_height = 0.2
+
+       # Handle 
+       self.handle_length = 1.5
+       self.handle_radius = 0.15
+
+       # Pommel 
+       self.pommel_radius = 0.2
+
+    def build_blade(self):
