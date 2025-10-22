0ce964dfbf62fac31e6b465e2ece5889ea99ecb1
CharlieC316 <chcarnahan@gmail.com>
2025-10-22 17:03:20 -0500

    made the build blade function to build the blade to the sword

--- src/P01A.py
+++ src/P01A.py
@@ -3,7 +3,7 @@ import maya.cmds as cmds
 class Sword:
     
     def __init__(self):
-        #parameters you can customize
+     #parameters you can customize
         # Blade
        self.blade_length = 5.0
        self.blade_width = 0.2
@@ -22,3 +22,11 @@ class Sword:
        self.pommel_radius = 0.2
 
     def build_blade(self):
+        blade_name, _ = cmds.polycube(height=self.blade_length, 
+                                      width=self.blade_width,
+                                      depth=self.blade_thickness,
+                                      name="blade1")
+        
+        # position of the blade vertically 
+        cmds.xform(blade_name, translation=[0, self.blade_length / 2.0 + self.gaurd_height, 0])
+        return blade_name
