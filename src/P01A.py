d5d40f2a7f304920e3f2bad42a0c81dbad269427
CharlieC316 <chcarnahan@gmail.com>
2025-10-22 16:53:40 -0500

    created a class and a TODO list

---
+++ src/P01A.py
@@ -0,0 +1,11 @@
+import maya.cmds as cmds
+
+class Sword:
+    
+    def __init__(self):
+        #parameters you can customize
+        # TODO
+        # Blade Length 
+        # Blade Width
+        # Blade Thinkness 
+        # Gaurd Height and weight 
